85cb564352613bf76a6ac63413a16db10ac7bd59
Isaac Tonyloi <tonyloimongare10@students.ku.ac.ke>
2020-11-18 06:22:04 -0800

    Create visualizations using matplotli.py

---
+++ visualizations using matplotli.py
@@ -0,0 +1,34 @@
+#python -mpip install -u matplotlib
+from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+ #matplotlib come with a number of plots
+
+from matplotlib import pyplot as plt
+
+
+x=[1,2,3,4,5,6]
+y=[12,34,45,45,34,23]
+plt.bar(x,y)
+plt.show()
+ 
+
+x=['KCB','COOP','NIC','FAMILY','BARCLAYS']
+y=[123,345,245,278,454]
+plt.bar(x,y)
+plt.show()
+
+#import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+plt.hist(y)
+plt.show()
+
+
+from matplotlib import pyplot as plt 
+import matplotlib.pyplot as plt
+x=['NAIROBI','KISUMU','MOMBASA','WAJIR','KISII']
+Y=[123,345,563,673,453,325]
+#scatterplot
+plt.scatter(x,y)
+plt.show()
+
+
