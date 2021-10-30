7c3dc3d314dbbc24be8dff86a5fe39637f783d72
isaactonyloi <tonyloimongare10@students.ku.ac.ke>
2021-10-29 18:31:03 -0700

    oop concept in Pythom

---
+++ creatinganobject.py
@@ -0,0 +1,23 @@
+class Dogs:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print("bark bark !!")
+
+    def info(self):
+        print(f'My name is {self.name} and i\'m ages {self.age}')
+
+
+ozzy = Dogs('simba', 25)
+obby = Dogs('Saddam', 90)
+
+#calling the methods
+ozzy.bark()
+ozzy.info()
+
+#calling the methods
+obby.bark()
+obby.info()
