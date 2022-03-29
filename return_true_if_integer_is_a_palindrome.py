d1762194e84ad1c0cb3f720a5605dd18cc568667
Isaac Tonyloi <70072644+tonymongare@users.noreply.github.com>
2022-03-29 02:03:40 -0700

    Create return_true_if_integer_is_a_palindrome.py
    Given an integer x, return true if x is palindrome integer.
    
    An integer is a palindrome when it reads the same backward as forward.
    
    For example, 121 is a palindrome while 123 is not.

---
+++ return_true_if_integer_is_a_palindrome.py
@@ -0,0 +1,19 @@
+def findPalindrome(x: int) -> bool:
+    
+    if x < 0:
+        return False
+        
+    number = x
+    reversedNumber = 0
+    
+    while number:
+        
+        lastDigit = number % 10
+        
+        reversedNumber = reversedNumber * 10 + lastDigit
+        
+        number //= 10
+    return reversedNumber == x
+    
+print(findPalindrome(123))
+print(findPalindrome(131))
