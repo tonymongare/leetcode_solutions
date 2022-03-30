83b3e7e919d8609cd7f887d0b9cdf16a0eb02334
Isaac Tonyloi <70072644+tonymongare@users.noreply.github.com>
2022-03-30 02:04:37 -0700

    Create merge_two_sorted_lists.py
    You are given the heads of two sorted linked lists list1 and list2.
    
    Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
    
    Return the head of the merged linked list.

---
+++ merge_two_sorted_lists.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        
+        if list1.val < list2.val:
+            temp = head = ListNode(list1.val)
+            list1 = list1.next
+        else:
+            temp = head = ListNode(list2.val)
+            list2 = list2.next
+            
+        while list1 is not None and list2 is not None:
+            if list1.val < list2.val:
+                temp.next = ListNode(list1.val)
+                list1 = list1.next
+            else:
+                temp.next = ListNode(list2.val)
+                list2 = list2.next
+                
+            temp = temp.next
+            
+            
+        while list1 is not None:
+            temp.next = ListNode(list1.val)
+            list1 = list1.next
+            temp = temp.next
+        while list2 is not None:
+            temp.next = ListNode(list2.val)
+            list2 = list2.next
+            temp = temp.next
+        return head
+    
+            
+            
+                
+                
+        
+       
+            
+        
+        
+    
+    
+        
