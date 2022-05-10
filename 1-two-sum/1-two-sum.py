62ff95a69a5c8e7075b78ba90e84dec2aad731e6
Isaac Tonyloi <70072644+tonymongare@users.noreply.github.com>
2022-05-09 23:36:01 -0700

    Time: 65 ms (85.05%), Space: 15.1 MB (49.77%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        new_hash_map = {}
+        
+        for i, n in enumerate(nums):
+            difference = target - n
+            if difference in new_hash_map:
+                return(i, new_hash_map[difference])
+            else:
+                new_hash_map[n] = i
+                
+            
+        
