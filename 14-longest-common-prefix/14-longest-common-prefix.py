f00aea007d6d509bc07cbc7240c3368a2865925d
Isaac Tonyloi <70072644+tonymongare@users.noreply.github.com>
2023-05-20 21:12:53 +0300

    Update 14-longest-common-prefix.py

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -2,7 +2,7 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
        
         empty_string = ""
-        //
+        /////
         if strs is None or len(strs) == 0:
             return empty_string
         
