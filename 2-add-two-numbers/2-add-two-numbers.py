99682f2e0259611ca99679dcfc5ed98dab2077b2
Isaac Tonyloi <70072644+tonymongare@users.noreply.github.com>
2023-05-21 20:44:51 +0300

    Update 2-add-two-numbers.py

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -1,7 +1,6 @@
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-      ////
         head = None
     # Reference of head which is null at this poin
         temp = None
